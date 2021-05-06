11e91db94641e9fe5aa43bc4de4ab91f635674ee
TranQuocHuy20CDTH41 <82139951+TranQuocHuy20CDTH41@users.noreply.github.com>
2021-05-06 13:23:11 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,33 @@
+package basic.dev;
+/**
+ * lab02
+ * Ghi chú (Comment nhiều dòng)
+ * @author TranQuocHuy
+ * 1.Tên lớp trùng tên file 
+ * 2.Tên lớp chữ cái đầu viết hoa. Nếu từ ghép thì những chữ cái đầu viết hoa
+ * 3.Tên lớp đặt phải có ý nghĩa. Ví dụ: class Person: Lớp mô tả con người
+ * - Interface: Phải có tiền tố I. Ví dụ: IListener
+ * - Abstract: Phải có tiền tố Abstract. Ví dụ:AbstractShape
+ *
+ */
+public class MainApp {
+	//Comment 1 dòng cách 1. Ctrl + /
+	/*Comment 1 dòng cách 2*/ 
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		/**
+		 * Cách đặt tên biến:
+		 * 1. Không chứa ký tự đặc biệt !@#$%^&*()-+=
+		 * 2. Không chứa khoảng trắng
+		 * 3. Viết thường
+		 * 4. Không được bắt đầu bằng số
+		 */
+		int number;
+		String nameSchool;
+	}
+	/*
+	 * tên phương thức tuân theo quy tắc camelCase
+	 */
+	public void show() {
+	}
+}
